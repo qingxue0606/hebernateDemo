58b3f4fe105ac08878e5c578a4b279d15d397e2c
xiangqingxue <77055664@qq.com>
2018-12-15 10:23:52 +0800

    增加了分页
    Signed-off-by: xiangqingxue <77055664@qq.com>

--- src/main/java/com/example/demo/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CustomerServiceImpl.java
@@ -9,6 +9,9 @@ import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.ExampleMatcher.GenericPropertyMatcher;
 import org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.dao.CustomerDao;
@@ -41,8 +44,10 @@ public class CustomerServiceImpl  implements CustomerService{
             .withMatcher("address", GenericPropertyMatchers.contains())   //包含匹配
             .withIgnorePaths("remark");   //可忽略
         Example<Customer> ex=Example.of(customer,matcher);
-        List<Customer> ls=customerDao.findAll(ex);
-        System.out.println(ls);
+        Pageable pageable=new PageRequest(0, 3);
+        Page<Customer> ls=customerDao.findAll(ex,pageable);
+        
+        System.out.println(ls.getContent());
     }
 
 }
